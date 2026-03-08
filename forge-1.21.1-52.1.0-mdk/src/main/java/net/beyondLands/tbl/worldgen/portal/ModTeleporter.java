4c4e979249e8e465d0f225ce56e47514b2c09119
Musicalement1 <musicalement.github@gmail.com>
2026-03-08 15:57:58 +0100

    GateOpener Portal v1.2

--- forge-1.21.1-52.1.0-mdk/src/main/java/net/beyondLands/tbl/worldgen/portal/ModTeleporter.java
+++ forge-1.21.1-52.1.0-mdk/src/main/java/net/beyondLands/tbl/worldgen/portal/ModTeleporter.java
@@ -4,6 +4,8 @@ import net.beyondLands.tbl.block.ModBlocks;
 import net.beyondLands.tbl.block.ModPortalBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.material.Fluids;
@@ -20,9 +22,24 @@ public class ModTeleporter {
         dest.setBlock(pos, ModBlocks.BL_PORTAL.get().defaultBlockState(), 3);
         dest.setBlock(pos.above(), Blocks.AIR.defaultBlockState(), 3);
         dest.setBlock(pos.above(2), Blocks.AIR.defaultBlockState(), 3);
+        return new DimensionTransition(
+                dest,
+                Vec3.atCenterOf(pos.above()),
+                Vec3.ZERO,
+                entity.getYRot(),
+                entity.getXRot(),
 
-        return new DimensionTransition(dest, Vec3.atCenterOf(pos.above()), Vec3.ZERO,
-                entity.getYRot(), entity.getXRot(),
-                DimensionTransition.PLACE_PORTAL_TICKET);
+                (e) -> {
+                    dest.playSound(
+                            null,
+                            pos,
+                            SoundEvents.ENDERMAN_TELEPORT,
+                            SoundSource.PLAYERS,
+                            1.0F,
+                            1.0F
+                    );
+                }
+
+        );
     }
 }
